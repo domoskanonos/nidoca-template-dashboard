4c3d76831a2fea03018bff937d64b8bd87a53b0a
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-06-05 11:35:05 +0200

    modified

--- source/i18n/import.ts
+++
@@ -1,7 +0,0 @@
-import messageAppDE from './message-de.json';
-import messageAppEN from './message-en.json';
-
-import {I18nService, LanguageKey} from '@domoskanonos/frontend-basis';
-
-I18nService.getUniqueInstance().addData(messageAppDE);
-I18nService.getUniqueInstance().addData(messageAppEN, LanguageKey.EN);
--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,24 @@
-import './pages/import';
-import './i18n/import';
+import messageAppDE from './i18n/message-de.json';
+import messageAppEN from './i18n/message-en.json';
+
+import {I18nService, LanguageKey} from '@domoskanonos/frontend-basis';
+
+I18nService.getUniqueInstance().addData(messageAppDE);
+I18nService.getUniqueInstance().addData(messageAppEN, LanguageKey.EN);
+
+import './pages/page-dashboard';
+import './page-settings';
+import './page-register';
+import './page-register-ok';
+import './page-login';
+import './page-logout';
+import './page-change-password';
+import './page-reset-password';
+import './page-terms-of-use';
+import './page-default';
+
 import './app';
+
 import './index.css';
 
 export * from '@domoskanonos/nidoca-app';
